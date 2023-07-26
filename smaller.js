e5e43eb2de5a85c3cd75a0563303d9f4612633b5
Sroyon <109625430+Sroyon@users.noreply.github.com>
2023-07-26 10:35:02 +0530

    Create smaller.js

---
+++ smaller.js
@@ -0,0 +1,22 @@
+ let a=10,b=20,c=30;
+    if(a<b)
+    {
+        if(a<c)
+        {
+            console.log("A is smaller with value ",a);
+        }else
+        {
+            console.log("C is smaller with value ",c);
+        }
+    }
+    else
+    {
+        if(b<c)
+        {
+            console.log("B is smaller with value ",b);
+        }
+        else
+        {
+            console.log("C is smaller with value ",c);
+        }
+    }
